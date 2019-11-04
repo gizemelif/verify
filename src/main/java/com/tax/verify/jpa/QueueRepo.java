1c106cc5baf50e743e18dccae8c687b06c2aca11
gizem <gizemelif.atalay@gvg.com.tr>
2019-11-04 12:03:24 +0300

    bug fixed.

--- src/main/java/com/tax/verify/jpa/QueueRepo.java
+++ src/main/java/com/tax/verify/jpa/QueueRepo.java
@@ -13,17 +13,14 @@ import java.util.Date;
 @Repository
 @org.springframework.transaction.annotation.Transactional(propagation = Propagation.REQUIRES_NEW)
 public interface QueueRepo extends JpaRepository< Queue, String > {
-    /*@Query(value = "SELECT * FROM SCHEDULED_VD_TC_INDEX u WHERE u.state = 'PROCESSING' ORDER BY u.created_at ASC LIMIT 1", nativeQuery = true)
-    Queue findProcessing();*/
-
-    @Query(value = "SELECT * FROM SCHEDULED_VD_TC_INDEX u WHERE u.state = 'WAITING' ORDER BY u.created_at ASC LIMIT 1", nativeQuery = true)
-    Queue findByState();
-
-    @Modifying(flushAutomatically = true)
+    @Modifying
     @Query("UPDATE Queue q SET q.state =:state, q.notification_mail =:notification_mail, q.start_date = current_timestamp where q.job_oid =:job_oid")
     void updateState(@Param("state")Queue.QueueState state, @Param("notification_mail") String notification_mail, @Param("job_oid") String job_oid);
 
-    @Modifying(flushAutomatically = true)
+    @Modifying
     @Query("UPDATE Queue q SET q.state =:state, q.notification_mail =:notification_mail, q.end_date =:end_date where q.job_oid =:job_oid")
     void updateStateProcessed(@Param("state")Queue.QueueState state, @Param("notification_mail") String notification_mail,@Param("end_date") Date end_date,@Param("job_oid") String job_oid);
+
+    @Query(value = "SELECT * FROM SCHEDULED_VD_TC_INDEX u WHERE u.state = 'WAITING' OR u.state = 'PROCESSING' ORDER BY u.created_at ASC LIMIT 1", nativeQuery = true)
+    Queue findByState();
 }
